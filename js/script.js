06769ebc279df5ccc483dcc21ee57121dd8ad1dc
maxirukus <31831342+maxirukus@users.noreply.github.com>
2024-09-08 14:51:40 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ function Page() {
             $historicDiv[n].classList.add('animated');
             $historicDiv[n].classList.add('slideInRight');
         }
-         xhttp.open('GET', 'https://api-v2.streamafrica.net/musicsearch?query=' + info.artist + ' ' + info.song + '&service=' + API_SERVICE.toLowerCase());
+        xhttp.open('GET', 'https://api-v2.streamafrica.net/musicsearch?query=' + info.artist + ' ' + info.song + '&service=' + API_SERVICE.toLowerCase());
         xhttp.send();
 
         setTimeout(function () {
